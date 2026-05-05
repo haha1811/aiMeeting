5d481b148ef4f6b79e7422404f5c31d5d60eb4cc
haha1811 <92005285+haha1811@users.noreply.github.com>
2026-05-05 22:23:44 +0800

    Add public module exports

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+export { Moderator } from "./moderator.js";
+export { DiscussionService } from "./service.js";
+export { JsonlDiscussionStore } from "./storage.js";
+export type {
+  AgentDescriptor,
+  AgentDiscussionContext,
+  AgentResponse,
+  AppendMessageInput,
+  CreateSessionInput,
+  DiscussionEvent,
+  DiscussionMessage,
+  DiscussionResult,
+  DiscussionSession,
+  DiscussionStatus,
+  HermesAgent,
+  TaskAssignment,
+  TaskAssignmentInput
+} from "./types.js";
